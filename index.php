72558876607b39e08583c792da4b07fcd12ae2fc
Evan Smith <55214117+Paancaakes@users.noreply.github.com>
2020-02-14 23:25:53 -0500

    added minimal code to get temporary satisfaction until I can actually work on my checklist

--- index.php
+++ index.php
@@ -5,6 +5,10 @@ include 'PHP/header.php';
 <section class="forum-container">
 
 Index
+<a href="game.php">game.php</a>
+<a href="login.php">login.php</a>
+<a href="page1.php">page1.php</a>
+<a href="registration.php">registration.php</a>
 
 
 </section>
