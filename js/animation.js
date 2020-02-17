ac24151043f343db5655985d7a501eba33e1e156
maxwelllucasr <55898589+maxwelllucasr@users.noreply.github.com>
2020-02-16 19:26:29 -0500

    Mouse position function

--- js/animation.js
+++ js/animation.js
@@ -1,6 +1,6 @@
 import Floater from '/cis273/js/floater.js';
 
-//constants
+// //constants
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 
@@ -26,26 +26,55 @@ yourObject.position.x = 150;
 yourObject.draw(ctx);
 
 
-// ctx.fillStyle = '#1f1';
-// ctx.fillRect(10,10,20,20)
+ctx.fillStyle = '#1f1';
+ctx.fillRect(10,10,20,20)
 
 
-canvas=$('#pad')[0]
-        context= canvas.getContext("2d")
-        pendown= false
+// canvas=$('#pad')[0]
+//         context= canvas.getContext("2d")
+//         pendown= false
         
-        $('#pad').mousemove(function(event)
-            {
-                xpos = event.pageX - canvas.offsetLeft
-                ypos = event.pageY - canvas.offsetTop
+//         $('#pad').mousemove(function(event)
+//             {
+//                 xpos = event.pageX - canvas.offsetLeft
+//                 ypos = event.pageY - canvas.offsetTop
                 
-                if(pendown) context.lineTo(xpos, ypos)
-                else        context.moveTo(xpos,  ypos)
+//                 if(pendown) context.lineTo(xpos, ypos)
+//                 else        context.moveTo(xpos,  ypos)
                 
-                context.stroke()
+//                 context.stroke()
             
             
-            })
+//             })
             
-            $('#pad').mousedown(function() {pendown = true})
-            $('#pad').mouseup(function() {pendown = false})
+//             $('#pad').mousedown(function() {pendown = true})
+//             $('#pad').mouseup(function() {pendown = false})
+//     var canvas = document.getElementById('gameWrapper');
+//   var context = canvas.getContext('2d');
+
+
+function writeMessage(canvas, message) {
+    var context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.font = '18pt Calibri';
+    context.fillStyle = 'black';
+    context.fillText(message, 1, 25);
+}
+
+  function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top
+    };
+  }
+
+  
+  canvas.addEventListener('mousemove', function(evt) {
+    var mousePos = getMousePos(canvas, evt);
+    var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
+    writeMessage(canvas, message);
+    console.log(message);
+    
+  }, false);
+
