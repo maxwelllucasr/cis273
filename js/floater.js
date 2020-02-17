78101f50c5701d0ab384f49f1cbf983b23eb0743
maxwelllucasr <55898589+maxwelllucasr@users.noreply.github.com>
2020-02-17 17:22:15 -0500

    huge changes

--- js/floater.js
+++ js/floater.js
@@ -1,5 +1,6 @@
 import {fraction} from "/cis273/js/frac.js";
 // import {HCF} from '/cis273/js/frac.js';
+import {createSlope} from '/cis273/js/functions.js';
 
 export default class Floater{
 
@@ -36,14 +37,32 @@ export default class Floater{
 
 
         //Takes decimal as input, outputs numerator as numAndDem[1] and denominator as numAndDem[2]
-        let decimalSlope = fraction(0.5);
-        let numAndDem = decimalSlope.split('/');
-        
+        // let decimalSlope = fraction(-5.5);
+        // // console.log(decimalSlope);
+        // let numAndDem = decimalSlope.split('/');
+        // console.log(numAndDem);
 
 
         // console.log(line.numerator + "/" + line.denominator);
-
-        this.position.x += 5 / deltaTime; //bad, fix later
+       
+       //x1, x2, y1, y2
+    //    var slope = null;
   
+        let slope = createSlope(10, 40, 20, 10);
+        
+
+       
+
+    //    console.log(slope);
+
+        // rise and run not only determine the angle, but the speed.
+        //For testing purposes
+        //rise over run, or y over x.  This would be 1.5 in decimal
+        let rise = slope[0]; //y
+        let run = slope[1]; //x
+
+       
+        this.position.x += run / deltaTime;
+        this.position.y += rise / deltaTime;
     }
 }
--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,47 @@
 
-function createSlope(x1,x2,y1,y2){
+export function createSlope(x1,x2,y1,y2){
 
     if ((x2-x1) == 0)
         return null;
-    else
-        return ((y2-y1)/(x2-x1));
+    else{
+        let y = y2-y1;
+        let x = x2-x1;
+
+        //Using this (somenumber/10^length-1) to reformat extreme numerators and denomiators to something in the 1-10 range.
+        
+        // console.log(y.toString().length);
+
+        // let yLeng = Math.pow(10,y.toString().length -1);
+        // let xLeng = Math.pow(10,x.toString().length -1);
+
+        // console.log(yLeng);
+
+        // let newX = x/xLeng;
+        // let newY = y/yLeng;
+
+        // console.log(decimalSlope);
+        let reduced = reduce(y,x);
+        // console.log(y, x);
+
+        y = reduced[0];
+        x = reduced[1];
+
+        console.log(y, x);
+
+        if(x < 0) {
+            y = -y;
+            x = -x;
+        }
+
+        return Array(y, x);
+
+    }
 }
 
+function reduce(numerator,denominator){
+    var gcd = function gcd(a,b){
+      return b ? gcd(b, a%b) : a;
+    };
+    gcd = gcd(numerator,denominator);
+    return [numerator/gcd, denominator/gcd];
+  }
