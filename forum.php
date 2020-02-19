4f07220883b3d1ca937c08e0d390742781ed0da9
CorruptedJCStudentFile <60949821+CorruptedJCStudentFile@users.noreply.github.com>
2020-02-18 19:24:23 -0500

    ạ̷̧̝̻̠͛̋͒ ̷͚̦̻̩̈͑̉͋̎$̴͍͈̬̩͚͉̄̌̌̽͆̌t̶̢̼̪͕̜̿͗̃̃̒͠a̶̩̫͙͉͍͌r̷͈͎͂́t̴͍̲̭̗̏͋̊̏͜i̵̧͕͚̜̼͌n̷͈̳̹̦̮̘̽g̵̻̼̉̑͊̓̆ͅ ̴̛̣̉̇͂͜p̷̹͉̽͒̀͜0̷̹̦̩̟̪͇̇̓̕͘i̵̱̩͈̳͗n̶̗̋̾͝t̷̼̼͐ ̸̡͓̬́̋͑̃̾f̴̭̹͖͚͖͆̈́́́͋̌ͅo̸̰̞̪̅̆̐͝r̸̢̰͇͇̉̄̃ ̷͓͈̓̚f̶̯̱̜̻̎͒͗͋̍͘ͅo̸̝̯͈̞͓̣͐̃̉̇̕r̷͓̟̻̓͋͜ù̴̢̊͆͜m̶̲̦̞̖̻͑͝.̴͉̫̓̽̓͂̈́p̷̥̬̞̃h̴̿̔̉̋̉͜p̸̱̖̯̗͊͐̆̌͜͠

--- forum.php
+++ forum.php
@@ -1,13 +1,16 @@
 <?php
 include 'PHP/header.php';
 ?>
-
+<?php 
+	echo $_POST["forumpost"]; 
+?>
 <section class="forum-container">
     <div class="container">
-
-    Forum
-
-
+		<form action="forum.php" method="post">
+			Forum
+			<input type="text" name="forumpost"/>
+			<input type="submit" value="Submit"/>
+		</form>
     </div>
 </section>
 
