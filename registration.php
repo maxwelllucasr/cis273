72199be0e6636e887c236735604c92043e1e0b17
Evan Smith <55214117+Paancaakes@users.noreply.github.com>
2020-02-21 01:16:23 -0500

    Update registration.php

--- registration.php
+++ registration.php
@@ -4,7 +4,21 @@ include 'PHP/header.php';
 
 <section>
     <div class="container">
-    Registration
+
+    	<h1>Register</h1>
+
+			<form action="verify.php" method = "post" autocomplete = "off">
+
+				<input type = "text" placeholder = "Username" name = "user" id="user" required>
+
+				<input type = "text" placeholder = "Password" name = "pass" id="pass" required>
+
+				<input type = "text" placeholder = "Retype Password" name = "pass2" id="pass2" required>
+
+				<input type = "email" name = "email" placeholder = "johnnyappleseed123@gmail.com">
+
+				<input type = "submit" value = "Register">
+
     </div>
 </section>
 
