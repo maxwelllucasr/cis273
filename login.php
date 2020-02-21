6fcf4cec2997395620d34f7acec2e7bf347b8ab9
Evan Smith <55214117+Paancaakes@users.noreply.github.com>
2020-02-20 19:59:22 -0500

    Update login.php

--- login.php
+++ login.php
@@ -5,8 +5,16 @@ include 'PHP/header.php';
 <section class="login-container">
     <div class="container">
 
-    Login
+    	<h1>Login</h1>
 
+		<form action="verify.php" method = "post">
+
+			<input type = "text" placeholder = "Username" name = "user" id="user" required>
+
+			<input type = "text" placeholder = "Password" name = "pass" id="pass" required>
+			
+		</form>
+	
     </div>
 </section>
 
