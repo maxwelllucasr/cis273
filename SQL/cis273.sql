edee708fa4a3d229c85178f237b04b66ecc37152
maxwelllucasr <55898589+maxwelllucasr@users.noreply.github.com>
2020-03-25 17:39:14 -0400

    update dummy sql

--- SQL/cis273.sql
+++ SQL/cis273.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 17, 2020 at 05:54 AM
+-- Generation Time: Mar 25, 2020 at 09:38 PM
 -- Server version: 5.6.37
--- PHP Version: 5.6.31
+-- PHP Version: 7.1.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -32,7 +32,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `pass` varchar(30) NOT NULL,
   `email` varchar(30) NOT NULL,
   `score` mediumint(8) unsigned NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
@@ -42,8 +42,31 @@ INSERT INTO `user` (`id`, `user`, `pass`, `email`, `score`) VALUES
 (1, 'luke', 'password', 'luke@lukesfakewebsite.com', 9999),
 (2, 'evan', 'password', 'evan@evansfakewebsite.com', 9999),
 (3, 'chase', 'password', 'chase@chasesfakewebsite.com', 9999),
-(4, 'tyler', 'password', 'tyler@tylersfakewebsite.com', 9999);
+(4, 'tyler', 'password', 'tyler@tylersfakewebsite.com', 9999),
+(5, 'darklord666', 'password', 'darklord@dark.com', 5);
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user` (`user`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `user_2` (`user`,`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
