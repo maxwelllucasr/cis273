43bc164c75985b6083de2376a4b7ca58b1e17366
Evan Smith <55214117+Paancaakes@users.noreply.github.com>
2020-04-09 18:47:05 -0400

    Update registrationSQL.php
    comments

--- PHP/registrationSQL.php
+++ PHP/registrationSQL.php
@@ -56,12 +56,15 @@ if (isset($_POST['registration-button'])){
 
                         require 'registrationEmailHandler.php';
 
+                        //Random 16 digit salt
                         $salt = random_bytes (16);
 
                         $link = mysqli_connect($host, $un, $pass, $db);
 
+                        //salt the pass
                         $salted= $salt.$_POST['pass'];
 
+                        //hash the salted pass
                         $hash = hash('sha256', $salted);
 
                         $result = $link->prepare("INSERT INTO `cis273`.`user` (`id`, `user`, `pass`, `email`, `score`) VALUES (NULL, ?, ?, ?, '0';");
