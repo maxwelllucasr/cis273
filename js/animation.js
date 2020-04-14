c4a80256b17fc9ad0d8c0b099427a99c5d860b17
lmg101 <60763779+lmg101@users.noreply.github.com>
2020-04-14 14:58:31 -0400

    Update animation.js

--- js/animation.js
+++ js/animation.js
@@ -236,7 +236,7 @@ function gameLoop(timestamp){
     //tower proximity check & projectile generation
     for (let i = 0; i < numberOfTowers; i++){
       for (let j = 0; j < metadata.currentBadguy; j++){
-
+        if(badguy[j].isDead==false){  
       if (distance(tower[i].position.x, badguy[j].position.x, tower[i].position.y, badguy[j].position.y) < towerProximity) 
       {
       
@@ -283,6 +283,7 @@ function gameLoop(timestamp){
       }
       }
     }
+    }
     for (let i = 0; i < projectile.length; i++){
       for (let j = 0; j < metadata.currentBadguy; j++){
 
@@ -327,7 +328,7 @@ function gameLoop(timestamp){
         for (let j = 0; j < metadata.currentBadguy; j++){
 
         if (distance(pathPoint[badguy[j].currentHeading].position.x, badguy[j].position.x, pathPoint[badguy[j].currentHeading].position.y, badguy[j].position.y) < pathPointProximity){
-          if(badguy[j].isDead==false){  
+   
 
           //No idea what this does, dont remember adding it, it breaks the game
           // if(badguy.length - 1 != badguy[j].currentHeading){ 
@@ -377,7 +378,7 @@ function gameLoop(timestamp){
 
           // }
           }
-        }
+        
         }
       // }
 
@@ -420,9 +421,9 @@ function gameLoop(timestamp){
       ctx2.drawImage(milkCarton, pathPoint[pathPoint.length-1].position.x, pathPoint[pathPoint.length-1].position.y,70,100);
 
   //Projectile update position
-  //for(let i = 0; i < projectile.length; i++){
-  //  projectile[i].updatePosition(deltaTime, pathPoint);
- // }
+  for(let i = 0; i < projectile.length; i++){
+    projectile[i].updatePosition(deltaTime, pathPoint);
+  }
 
   ctx2.fillStyle = "#999999";
   //Draw redraws the object... 
@@ -441,13 +442,13 @@ function gameLoop(timestamp){
   }
   else if (metadata.winCondition){
     gameMessage("You Won",ctx2,GAME_WIDTH,GAME_HEIGHT);
-  }
+ }
 
   //If all badguys are spawned
   if (numberOfBadGuys == metadata.currentBadguy){
     //Loop through all bad guys
     for(let i = 0; i < numberOfBadGuys; i++){
-      if (badguy[i].isDead) metadata.winCondition = true;
+      if (badguy[j].isDead) metadata.winCondition = true;
     }
 
   }
