d5eb4df2003d85c3283860448f0273cc609c6292
CorruptedJCStudentFile <60949821+CorruptedJCStudentFile@users.noreply.github.com>
2020-04-14 21:02:43 -0400

    f̴̝̉o̷͚͂r̷͓̓ ̵̳̊ù̵͇s̶̜̃ȅ̶̘ ̸͇͐w̷̡̌î̶̱t̸̡̊h̴͔̆ ̴̪́f̴̛͈õ̸ͅr̶̻̿ȕ̷͍m̷̡̏.̷̫́p̸͈̑h̷̪̍p̵̚ͅ

---
+++ js/charLimit.js
@@ -0,0 +1,25 @@
+//jQuery(document).ready(function($){
+
+	//jQuery('#forumTextArea').on('input', function() {
+		//var textarea = document.getElementById("forumTextArea");
+		//var length = textarea.length;
+		//var charactersLeft = 400 - length; 
+	//	console.log(charactersLeft);
+	//});
+	
+	//$('#forumTextArea').on('input', function() {
+	//	console.log("Function Test");
+	//});
+
+//console.log("Test");
+//});
+jQuery(document).ready(function($){
+    var textarea = document.getElementById("forumTextArea");
+	textarea.addEventListener("keypress", function(){
+		//console.log("keypress in textarea")
+		var length = textarea.value.length;
+        var charactersLeft = 400 - length;
+		console.log(charactersLeft);
+
+	}, false);
+});
