fe2047588a9a2b2c7c60b3cb6999a8c52e917221
maxwelllucasr <55898589+maxwelllucasr@users.noreply.github.com>
2020-04-21 18:50:59 -0400

    Redo highscore handler sql

--- PHP/highscoreController.php
+++ PHP/highscoreController.php
@@ -20,20 +20,30 @@ if (isset($_POST['score'])){
     if($_SESSION['loggedin']){
         //Connect, query
         $link = mysqli_connect($host, $un, $pass, $db);
-        $query = "SELECT * FROM `user` WHERE `user` = \"" . $_SESSION['user'] . "\";";
+
+        $result = $link->prepare("SELECT user, score FROM `user` WHERE `user` = ?;");
+        $result->bind_param('s',$_SESSION['user']);
+        $result->execute();
+        $result->store_result();
         $result = $link->query($query);
+
         if ($link->connect_error) {
             die("Connection failed: " . $link->connect_error);
         } 
 
         //only proceed if exactly one result
         if ($result->num_rows == 1) {
-            $row = $result->fetch_assoc();
-            $userHighScore = $row['score'];
+            $result->bind_result($user, $score);
+            $result->fetch();
+            // $userHighScore = $row['score'];
 
-            if ($userHighScore < $_POST['score']){
+            if ($score < $_POST['score']){
                 //update database high score if current post > one in db
-                $query = "UPDATE `user` SET `score` = ".$_POST['score']." WHERE `user` = \"".$_SESSION['user']."\";";
+                $result = $link->prepare("UPDATE `user` SET `score` = ? WHERE `user` = ?;");
+                $result->bind_param('is',$_POST['score'],$user);
+                $result->execute();
+                $result->store_result();
+
                 $link->query($query); //no need for result, just sending here
                 if ($link->connect_error) {
                     die("Connection failed: " . $link->connect_error);
