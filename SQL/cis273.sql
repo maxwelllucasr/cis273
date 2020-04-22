9b0e49a5338dad0869fa72e3b930f9075c1f1d88
Evan Smith <55214117+Paancaakes@users.noreply.github.com>
2020-04-21 20:09:17 -0400

    Update cis273.sql

--- SQL/cis273.sql
+++ SQL/cis273.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.15.9
--- https://www.phpmyadmin.net
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Mar 26, 2020 at 02:42 AM
--- Server version: 5.6.37
--- PHP Version: 7.1.8
+-- Host: 127.0.0.1
+-- Generation Time: Apr 22, 2020 at 02:07 AM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -26,7 +28,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `forum`
 --
 
-CREATE TABLE IF NOT EXISTS `forum` (
+CREATE TABLE `forum` (
   `id` int(11) NOT NULL,
   `author` varchar(255) NOT NULL,
   `date` date NOT NULL,
@@ -39,26 +41,28 @@ CREATE TABLE IF NOT EXISTS `forum` (
 -- Table structure for table `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` smallint(5) unsigned NOT NULL,
+CREATE TABLE `user` (
+  `id` smallint(5) UNSIGNED NOT NULL,
   `user` varchar(30) NOT NULL,
   `pass` varchar(30) NOT NULL,
   `email` varchar(30) NOT NULL,
-  `score` mediumint(8) unsigned NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+  `score` mediumint(8) UNSIGNED NOT NULL,
+  `salt` int(16) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `user`, `pass`, `email`, `score`) VALUES
-(1, 'luke', 'password', 'luke@lukesfakewebsite.com', 9999),
-(2, 'evan', 'password', 'evan@evansfakewebsite.com', 9999),
-(3, 'chase', 'password', 'chase@chasesfakewebsite.com', 9999),
-(4, 'tyler', 'password', 'tyler@tylersfakewebsite.com', 9999),
-(5, 'darklord666', 'password', 'darklord@dark.com', 5),
-(6, 'Darklord667', 'password2', 'password2', 5),
-(7, 'username1', 'password1', 'r@r.com', 0);
+INSERT INTO `user` (`id`, `user`, `pass`, `email`, `score`, `salt`) VALUES
+(1, 'luke', 'password', 'luke@lukesfakewebsite.com', 9999, NULL),
+(2, 'evan', 'password', 'evan@evansfakewebsite.com', 9999, NULL),
+(3, 'chase', 'password', 'chase@chasesfakewebsite.com', 9999, NULL),
+(4, 'tyler', 'password', 'tyler@tylersfakewebsite.com', 9999, NULL),
+(5, 'darklord666', 'password', 'darklord@dark.com', 5, NULL),
+(6, 'Darklord667', 'password2', 'password2', 5, NULL),
+(7, 'username1', 'password1', 'r@r.com', 0, NULL),
+(10, 'userover8', 'e694eafa1ce6a5038781a151ed3287', 'register@user.com', 0, NULL);
 
 --
 -- Indexes for dumped tables
@@ -88,11 +92,14 @@ ALTER TABLE `user`
 --
 ALTER TABLE `forum`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
+  MODIFY `id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
